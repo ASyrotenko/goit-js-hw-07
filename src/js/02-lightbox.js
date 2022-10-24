2bc50af8b023442e6a430ecacc01e64a35b97593
Andrey <andreysyrotenko@gmail.com>
2022-10-24 20:55:47 +0300

    Task 2 add src and markup

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imgContainer = document.querySelector('.gallery');
+const imgMarkup = createImgMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+imgContainer.addEventListener('click', onImgContainerClick);
+
+function createImgMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <li>
+        <a class="gallery__item" href="${original}"">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+    })
+    .join('');
+}
+
+function onImgContainerClick(evt) {
+  evt.preventDefault();
+}
